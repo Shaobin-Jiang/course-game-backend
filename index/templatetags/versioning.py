3001284e592d7905adf75d5f146a43cd1703b85b
Shaobin-Jiang <shaobin-jiang@outlook.com>
2023-02-10 09:57:56 +0800

    modify: apply versioning for refreshing cached static files

---
+++ index/templatetags/versioning.py
@@ -0,0 +1,7 @@
+from django import template                                                                                                              
+
+register = template.Library()                                                                                                            
+
+@register.simple_tag(name='versioning')                                                                                                  
+def versioning():                                                                                                                        
+    return '0.0.1.20230210-beta'
