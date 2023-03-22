76d14eaaea95d0ba9125fd7210901737453bc2b3
Shaobin-Jiang <shaobin-jiang@outlook.com>
2023-03-22 16:41:51 +0800

    modify: add 'headquarter' boolean field to user model

--- user/models.py
+++ user/models.py
@@ -7,6 +7,7 @@ class Player(AbstractUser):
     is_active = models.BooleanField(verbose_name='是否注册', default=False)
     full_name = models.CharField(verbose_name='姓名', max_length=16, blank=True)
     student_class = models.CharField(verbose_name='班级', max_length=16, blank=True)
+    headquarter = models.BooleanField(verbose_name='是否本部', default=True)
 
     def __str__(self):
         return self.username
