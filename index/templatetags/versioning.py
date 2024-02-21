ff09ce796a4291624ad42e0e2bc091e22ff5753f
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-02-21 16:49:17 +0800

    20240221 update - v0.1.0

--- index/templatetags/versioning.py
+++ index/templatetags/versioning.py
@@ -4,4 +4,4 @@ register = template.Library()
 
 @register.simple_tag(name='versioning')                                                                                                  
 def versioning():                                                                                                                        
-    return '0.0.2.20230414-beta'
+    return '0.1.0.20240221'
