b6c71f4fc3b3c23eda0127e9bd73c88e167b00a2
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-02-29 10:36:58 +0800

    update version number

--- index/templatetags/versioning.py
+++ index/templatetags/versioning.py
@@ -4,4 +4,4 @@ register = template.Library()
 
 @register.simple_tag(name='versioning')                                                                                                  
 def versioning():                                                                                                                        
-    return '0.1.0.20240221'
+    return '0.1.1.20240229'
